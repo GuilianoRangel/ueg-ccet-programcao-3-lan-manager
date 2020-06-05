38ac1674551bbdf63308a7ff5c539ea69a5248e8
GuilianoRangel <guiliano@gmail.com>
2020-06-04 21:00:57 -0300

    Fim Aula 04/06/2020 Metodo de teste de listar tudo e ajuste conforme alterações da arquitetura.

--- src/main/java/modelo/Tipo.java
+++ src/main/java/modelo/Tipo.java
@@ -1,5 +1,38 @@
 package modelo;
 
-public class Tipo extends Tabela<Integer> {
+import lombok.Data;
+import model.Table;
 
+import java.util.Arrays;
+import java.util.List;
+
+public @Data class Tipo extends Table {
+    private String nome;
+
+    public List<String> getColumnNames() {
+        return Arrays.asList("idTipo", "tipo");
+    }
+
+    public String getTableName() {
+        return "tipo";
+    }
+
+    public Table getNewTableObject() {
+        return new Tipo();
+    }
+
+    public boolean setTableColumnValues(List<Object> list) {
+        this.setPk((Integer) list.get(0));
+        this.setNome((String) list.get(1));
+        return true;
+    }
+
+    public String getPkColumnName() {
+        return "idTipo";
+    }
+
+    public boolean usePkInInsert() {
+        //TODO é só para teste
+        return true;
+    }
 }
--- src/test/java/TesteTipo.java
+++ src/test/java/TesteTipo.java
@@ -1,7 +1,20 @@
 import modelo.Tipo;
+import persistence.Dao;
+import persistence.IDao;
 
 public class TesteTipo {
     public static void main(String[] args) {
+        //testeModelo();
+        testeListAll();
+
+    }
+
+    private static void testeListAll() {
+        IDao dao = new Dao(new Tipo());
+        System.out.println(dao.listAll());
+    }
+
+    private static void testeModelo() {
         Tipo tipo = new Tipo();
         tipo.setPk(10);
         tipo.setNome("Teste");
